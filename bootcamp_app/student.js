f3ab4fe806aea0bf788a69fea76d65579b98bba7
Victor Anogwih <anogwihvictor@gmail.com>
2024-02-26 10:41:57 -0500

    parameterised_queries_used_in_students.js_and_teachers.js

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -7,15 +7,24 @@ const pool = new Pool({
   database: 'labber'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
+const studentQuery = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+const values =  [cohortName, limit];
+
+pool.query(studentQuery, values, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-}).catch(err => console.error('query error', err.stack));
+});
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,17 +6,26 @@ const pool = new Pool({
   host: 'localhost',
   database: 'labber'
 });
-pool.query (`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
+
+const teachersQuery = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`;
+
+const cohortName = process.argv[2];
+const values = [cohortName];
+
+pool.query(teachersQuery, values, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   res.rows.forEach(item => {
     console.log(`${item.cohort} : ${item.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+  });
+});
