28f667a545c115d188234bf14821c09ff70c2ac1
Victor Anogwih <anogwihvictor@gmail.com>
2024-02-26 09:36:34 -0500

    Quering_the_app_database

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'labber'
+});
+pool.query (`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(item => {
+    console.log(`${item.cohort} : ${item.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
